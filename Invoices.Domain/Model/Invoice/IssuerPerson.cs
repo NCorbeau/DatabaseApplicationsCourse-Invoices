a6cd6dffe8194cdc5a433b4a6d39e97e17555945
Maciej Głownia <glownia@gmail.com>
2015-03-19 18:00:32 +0100

    Added Roles class

--- Invoices.Domain/Model/Invoice/IssuerPerson.cs
+++ Invoices.Domain/Model/Invoice/IssuerPerson.cs
@@ -7,6 +7,7 @@ namespace Invoices.Domain.Model.Invoice
 {
     public class IssuerPerson
     {
+        public Role Role { get; set; }
         public string IssuerId { get; set; }
         public string Name { get; set; }
     }
